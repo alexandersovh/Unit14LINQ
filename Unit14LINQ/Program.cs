0b40db8771a459134cd9d1c7898000926f3f5dfa
Alexandr <alexandrsivoh@gmail.com>
2022-02-09 19:40:55 +0300

    Добавьте файлы проекта.

---
+++ Unit14LINQ/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Unit14LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Игорь", "Артемов", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedStuds = phoneBook.OrderBy(p => p.Name).ThenBy(ln => ln.LastName);
+
+
+
+            foreach (var stud in sortedStuds)
+                Console.WriteLine(stud.Name + " " + stud.LastName + ", " + stud.PhoneNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
